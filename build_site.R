63c513063096cad4434351b32363addb5c9ca62b
Samantha Csik <samanthacsik@gmail.com>
2021-09-01 21:53:04 -0700

    added annnotation with link to jhelvy github

--- build_site.R
+++ build_site.R
@@ -1,3 +1,6 @@
+# code taken from John Helvy
+  # https://github.com/jhelvy/jhelvy.com/blob/master/build_site.R
+
 source(file.path("R", "functions.R"))
 
 # Fist build the footer to capture today's date
